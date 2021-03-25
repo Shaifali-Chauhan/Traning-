73f7eb8212681a8d55f1d9dee2213411304cc7bd
Shaifali-Chauhan <81349700+Shaifali-Chauhan@users.noreply.github.com>
2021-03-25 13:23:21 +0530

    my first project

---
+++ FirstProject/src/main/java/com/example/demo/FirstProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+		
+		//System.out.println();
+		System.out.println("hello shaifali welcome to Spring boot project on STS");
+		
+	}
+
+}
