6322b254ee55cd51c9f3d35d92c3ecb29b339312
Shaifali-Chauhan <81349700+Shaifali-Chauhan@users.noreply.github.com>
2021-03-26 17:41:04 +0530

    Add files via upload

---
+++ bootjpa2example/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(104,'Abc','dv');
+insert into alien values(102,'def','dv');
+insert into alien values(103,'ghi','dv');
+insert into alien values(101,'jkl','dv');
+insert into alien values(105,'mno','dv');
